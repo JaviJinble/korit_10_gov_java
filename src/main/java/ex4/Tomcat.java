fdc3a700f00fc0d18296139549a6e804992d5033
JaviJinble <bubaragi23@gmail.com>
2026-04-17 16:53:41 +0900

    체이링 패턴 filterChain

---
+++ src/main/java/ex4/Tomcat.java
@@ -0,0 +1,13 @@
+package ex4;
+
+public class Tomcat {
+    public static void main(String[] args) {
+        System.out.println("요청들어옴");
+        ServletRequest request = new ServletRequest();
+        ServletResponse response = new ServletResponse();
+        FilterChain filterChain = new FilterChain();
+        filterChain.doFilter(request, response);
+        System.out.println("여기서 최종 응답");
+
+    }
+}
